968b2b6b67fcd9608212fb91d7a7c384b522989c
klfou <klfou@outlook.com>
2024-12-30 17:21:27 +0800

    init

---
+++ main.js
@@ -0,0 +1,29 @@
+// main.js
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+// Error Handling
+process.on('uncaughtException', (error) => {
+  console.error("Unexpected error: ", error);
+});
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      enableRemoteModule: false,
+    }
+  });
+  win.loadURL('http://localhost:4200');
+}
+// App Lifecycle
+app.whenReady().then(createWindow);
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
